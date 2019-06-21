682c9927345c0dcd56c0f007298c4f6f5b7d4071
豆朋伟 <doupengwei@hikvision.com>
2019-06-21 14:02:32 +0800

    移动工程，增加方法区oom的设置，jdk8开始放弃了PermGen，使用MateSpace代替， jdk9中完全删除

--- src/main/java/com/doudou/jvm/chapter2/JavaMethodAreaOOM.java
+++ src/main/java/com/doudou/jvm/chapter2/JavaMethodAreaOOM.java
@@ -9,6 +9,16 @@ import java.lang.reflect.Method;
 
 /**
  * VM Args: -XX:PermSize=10M -XX:MaxPermSize=10M
+ *
+ * When starting Java with "-XX:PermSize" and "-XX:MaxPermSize" on Linux and Mac, the following fatal error is issued:
+ *
+ * Unrecognized VM option 'MaxPermSize=384m'
+ * Error: Could not create the Java Virtual Machine.
+ * Error: A fatal exception has occurred. Program will exit.
+ *
+ * When starting on Windows, these options are ignored (as with JDK 8) and the Java application starts successfully.
+ *
+ * VM Args: -XX:MetaspaceSize=10M -XX:MaxMetaspaceSize=10M
  * @author 豆豆
  * @date 2019/6/20 20:48
  * @flag 以万物智能，化百千万亿身
@@ -32,10 +42,13 @@ public class JavaMethodAreaOOM {
                     return methodProxy.invokeSuper(obj, args);
                 }
             });
+           enhancer.create();
+
         }
     }
 
-    static class OOMObject{
+    static class OOMObject {
+
 
     }
 
